4418a8aefdd53a48c520fdbf0bf205f06bd9d570
Drew Davis <drew@Drews-MacBook-Pro.local>
2018-06-04 13:25:52 -0400

    Added client / GUI files to this repo.

---
+++ client/Schedule_App/ServerInteraction.swift
@@ -0,0 +1,250 @@
+//
+//  ServerInteraction.swift
+//  Schedule_App
+//
+//  Created by Drew Davis on 5/22/18.
+//  Copyright © 2018 Drew. All rights reserved.
+//
+// Code taken from main.swift of Drew_cmdLine project
+
+import Foundation
+
+
+class client {
+    
+    var requestPOST: URLRequest
+    var requestGET: URLRequest
+    
+    
+    let ID = String( arc4random_uniform(_:10000000) )
+    var urlString: String
+    var JSON_encoder: JSONEncoder
+    
+    let url: URL?
+    
+    // var cmdStr: String?
+    // var toPrintFromServer: String
+    var infoType : String?
+    var nextActivites : [String]?
+    var maxIdleTime : String?
+    var debugInfo : [String]?
+
+
+    /*
+     * Constructor
+    */
+    init() {
+        do {
+            //toPrintFromServer = ""
+            infoType = ""
+            nextActivites = []
+            maxIdleTime = ""
+            debugInfo = []
+            
+            JSON_encoder = JSONEncoder()
+            JSON_encoder.outputFormatting = .prettyPrinted
+            //var cmdStr = ""
+            
+            //let ID = String( arc4random_uniform(_:10000000) )
+            
+            //Implementing URLSession
+            urlString = "http://localhost:8080/" + ID // where the data is stored
+            //            guard let url = URL(string: urlString) else {
+            //                throw MyError.runtimeError("on no")
+            //            } // convert string to URL class
+            
+            url = URL(string: urlString)
+            
+            requestPOST = URLRequest(url: url!)
+            requestPOST.httpMethod = "POST"
+            requestGET = URLRequest(url: url!)
+            requestGET.httpMethod = "GET"
+            
+            
+            // tell server you are starting a new session
+//            // TXT bsased JSON format to send to server
+//            var someData = try JSON_encoder.encode(
+//                putCMD(cmd: "cmd",value: "RESTART",clientID: ID))
+            //GUI based JSON format to send to server
+            var aPut = putCMD()
+            aPut.infoType = "startup"
+            aPut.clientID = ID
+            
+            let someData = try JSON_encoder.encode( aPut )
+            requestPOST.httpBody = someData
+            
+            makeAReq(req: requestPOST)
+        } catch {
+            print("Error: client init() failed")
+        }
+    }
+    
+    
+    /*
+     * Destructor
+    */
+    deinit {
+        infoType = ""
+        nextActivites = []
+        maxIdleTime = ""
+        debugInfo = []
+    }
+    
+    
+//    // old method for text-based interface
+//    func sendStrToServer(str: String) {
+//        var aReq: URLRequest
+//        aReq = URLRequest(url: self.url!)
+//        aReq.httpMethod = "POST"
+//
+//        // tell server you are starting a new session
+//        do {
+//            let aData = try JSON_encoder.encode(
+//            putCMD(cmd: "cmd",value: str,clientID: self.ID))
+//            aReq.httpBody = aData
+//            makeAReqTXT(req: aReq)
+//        } catch let error as NSError {
+//            print(error)
+//        }
+//    }
+    
+    /*
+     * Send a JSON file to the server using internal methods
+     * This should be called by GUI items upon user interaction
+    */
+    func sendStructToServer(_ thePut: putCMD) {
+        do {
+            let json = try JSON_encoder.encode(thePut)
+            
+            var aReq: URLRequest
+            aReq = URLRequest(url: self.url!)
+            aReq.httpMethod = "POST"
+            aReq.httpBody = json
+            
+            makeAReq(req: aReq)
+        } catch let error as NSError {
+            print(error)
+        }
+    }
+    
+    
+//    // method for text-based UI
+//    /*
+//     * Make a Get or Put request to the server and get the response from the server
+//     * Server expected response JSON format defined by fromServer struct
+//    */
+//    func makeAReq(req: URLRequest) {
+//        URLSession.shared.dataTask(with: req) { (data, response, error) in
+//            if error != nil {
+//                print("nill error:")
+//                print(error!.localizedDescription)
+//            }
+//
+//            // These struct variables need to match the names seen in the JSON object
+//            struct fromServer: Codable {
+//                let toPrint: String
+//            }
+//
+//            // check for toPrint values in the response JSON
+//            guard let data = data else { return }
+//
+//            do {
+//                let servData = try JSONDecoder().decode(fromServer.self, from: data)
+//                //print("to print from the server: " + servData.toPrint)
+//                if servData.toPrint != "" {
+//                    print(servData.toPrint)
+//                    self.toPrintFromServer += servData.toPrint
+//                }
+//
+//            } catch let error as NSError {
+//                print(error)
+//            }
+//
+//            }.resume()
+//        //End implementing URLSession
+//    }
+    
+    /*
+     * Make a Get or Put request to the server and get the response from the server
+     * Server expected response JSON format defined by fromServer struct
+     * ^^ this response format should match the JSON format seen in the server code
+     */
+    // method for text-based UI
+    func makeAReq(req: URLRequest) {
+        URLSession.shared.dataTask(with: req) { (data, response, error) in
+            if error != nil {
+                print("nill error:")
+                print(error!.localizedDescription)
+            }
+            
+            // These struct variables need to match the names seen in the JSON object
+            struct fromServer: Codable {
+                // do not make these optional, because we want to throw error if some of the data is missing
+                // (unecesary components should be included as blank strings / empty vectors)
+                var infoType : String
+                var nextActivities : [String]
+                var maxIdleTime : String
+                var debugInfo : [String]
+            }
+            
+            // check for toPrint values in the response JSON
+            guard let data = data else { return }
+            
+            do {
+                let servData = try JSONDecoder().decode(fromServer.self, from: data)
+                // do something with servData
+                self.infoType = servData.infoType
+                self.nextActivites = servData.nextActivities
+                self.maxIdleTime = servData.maxIdleTime
+                
+            } catch let error as NSError {
+                print(error)
+            }
+            
+            }.resume()
+        //End implementing URLSession
+    }
+    
+    
+    /*
+     * Every 200 ms poll the server for anything to print using GET requests
+     */
+    func heartbeat() {
+        while(true) {
+            self.makeAReq(req: self.requestGET)
+            usleep(200000) // microseconds
+            if self.infoType == "startup" {return;}
+        }
+    }
+    
+    
+    enum MyError: Error {
+        case runtimeError(String)
+    }
+    
+}
+
+
+
+/*
+ * Structure of JSON format sent to the server
+ * Should be the same format as the JSONs passed into sendJSONtoServer()
+ */
+struct putCMD: Codable {
+    var clientID = ""
+    var infoType = ""  // options: startup / ganttRequest / confirmActivity / addActivity / removeActivity / editActivty
+    var activityName = ""
+    var debugInfo = [""]
+    
+    //        var cmd: String
+    //        var value: String
+    //        var clientID: String
+}
+
+// sample initialization
+//var aPut = putCMD()
+//aPut.clientID = ""
+//aPut.infoType = ""  // options: startup / ganttRequest / confirmActivity / addActivity / removeActivity / editActivty
+//activityName = ""
+//aPut.debugInfo = [""]
+
---
+++ client/Schedule_App/ViewController.swift
@@ -0,0 +1,180 @@
+//
+//  ViewController.swift
+//  Schedule_App
+//
+//  Created by Drew Davis on 5/21/18.
+//  Copyright © 2018 Drew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    var aClient: client?
+    var timer: DispatchSourceTimer?
+    
+    
+    //MARK: Properties
+//    @IBOutlet weak var titleText: UILabel!
+//    @IBOutlet weak var textInput: UITextField!
+    
+    @IBOutlet weak var MainTextIn: UITextField!
+    @IBOutlet weak var MainTextDisplay: UITextView!
+    @IBOutlet weak var ActivityOptionsStack: UIStackView!
+    @IBOutlet weak var UndoLastButton: UIButton!
+    @IBOutlet weak var VisualizeScheduleButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Handle the text field’s user input through delegate callbacks.
+        //textInput.delegate = self
+        MainTextIn.delegate = self
+        
+        self.aClient = client()
+        DispatchQueue.global(qos: .background).async {
+            //self.aClient!.continuouslyGetInOut() // wont leave this point until connection with server terminated
+            self.aClient!.heartbeat() // will update the member variables of aClient in background when the server sends info
+        }
+        
+        DispatchQueue.global(qos: .background).async {
+//            var strFromServ = ""
+            var buttons: [UIButton] = []
+            
+            while (true) {
+                sleep(1)
+                if (self.aClient!.infoType != "") {
+                    
+                    
+                    // Update the UI on the main thread
+                    DispatchQueue.main.async() {
+//                        strFromServ = self.aClient!.toPrintFromServer
+//                        self.aClient!.toPrintFromServer = ""
+                        let infoTypeFromServ = self.aClient!.infoType!; self.aClient!.infoType = ""
+                        let nextActsFromServ = self.aClient!.nextActivites!; self.aClient!.nextActivites = []
+                        let maxIdleTimeFromServ = self.aClient!.maxIdleTime!; self.aClient!.maxIdleTime = ""
+                        let debugInfoFromServ = self.aClient!.debugInfo!; self.aClient!.debugInfo = []
+                        
+                        
+                        
+                        // remove old button activity options
+                        for index in 0..<buttons.count {
+                            buttons[index].removeFromSuperview()
+                        }
+                        
+                        
+                        self.MainTextDisplay.text = self.MainTextDisplay.text! + infoTypeFromServ + " " + maxIdleTimeFromServ
+                        let range = NSMakeRange(self.MainTextDisplay.text.count - 1, 1);
+                        self.MainTextDisplay.scrollRangeToVisible(range)
+
+                        
+                        var strActs = nextActsFromServ // array of activities
+                        var firstActLine = 9999999999999
+                        
+                        if strActs.count > 0 {
+                            buttons = []
+                            for index in 0...strActs.count-1 {
+                                let button = UIButton(frame: .zero)
+                                button.setTitleColor(.black, for: .normal)
+                                button.setTitle(strActs[index], for: .normal)
+                                button.layer.borderColor = UIColor.gray.cgColor
+                                button.layer.borderWidth = 1.0
+                                button.translatesAutoresizingMaskIntoConstraints = false
+                                self.ActivityOptionsStack.addSubview(button)
+//                                button.frame.size.width = 200.0
+//                                    = CGSize(width:200, height:100)
+                                let widthContraints =  NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 150)
+                                let heightContraints = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 50)
+                                NSLayoutConstraint.activate([heightContraints,widthContraints])
+                                button.addTarget(self, action: #selector(self.activityButtonAction), for: .touchUpInside)
+                                buttons.append(button)
+                            }
+                            
+                            for index in 0...strActs.count-1 {
+                                let button = buttons[index]
+                                if index == 0 {
+                                    button.leadingAnchor.constraint(equalTo: self.ActivityOptionsStack.leadingAnchor, constant: 0.0).isActive = true
+                                    button.topAnchor.constraint(equalTo: self.ActivityOptionsStack.topAnchor, constant: 0.0).isActive = true
+                                } else if (index == 1) {
+                                    let preButton = buttons[0]
+                                    button.trailingAnchor.constraint(equalTo: self.ActivityOptionsStack.trailingAnchor, constant: 0).isActive = true
+                                    button.topAnchor.constraint(equalTo: preButton.topAnchor, constant: 0).isActive = true
+                                } else if (index % 2 == 1) { // odd indices go on right side
+                                    let preButton = buttons[index - 2]
+                                    button.leadingAnchor.constraint(equalTo: preButton.leadingAnchor, constant: 0).isActive = true
+                                    button.topAnchor.constraint(equalTo: preButton.bottomAnchor, constant: 10).isActive = true
+                                } else if (index % 2 == 0) { // even indices go on left side
+                                    let preButton = buttons[index - 2]
+                                    button.trailingAnchor.constraint(equalTo: preButton.trailingAnchor, constant: 0).isActive = true
+                                    button.topAnchor.constraint(equalTo: preButton.bottomAnchor, constant: 10).isActive = true
+                                }
+                                
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // called upon releasing first responder status (AKA right after testFieldShouldReturn returns)
+    func textFieldDidEndEditing(_ textField: UITextField) {
+//        self.aClient!.sendStrToServer(str: MainTextIn.text!) // TXT-based interface command
+        
+        var aPut = putCMD()
+        aPut.clientID = self.aClient!.ID
+        aPut.infoType = "confirmActivity"
+        aPut.activityName = self.MainTextDisplay.text
+        self.aClient!.sendStructToServer( aPut )
+        
+        self.MainTextDisplay.text = ""
+        MainTextIn.text = ""
+    }
+    
+    //MARK: Actions
+//    @IBAction func setDefaultTextButton(_ sender: UIButton) {
+//        titleText.text = "Default Text"
+//    }
+    
+    @IBAction func activityButtonAction(_ sender: UIButton) {
+
+        var aPut = putCMD()
+        aPut.clientID = self.aClient!.ID
+        aPut.infoType = "confirmActivity"
+        aPut.activityName = sender.titleLabel!.text!
+        self.aClient!.sendStructToServer(aPut)
+        
+        self.MainTextDisplay.text = ""
+    }
+    
+//    // no longer a feature in GUI interface
+//    @IBAction func undoButtonAction(_ sender: UIButton) {
+//        self.aClient!.sendStrToServer(str: "U")
+//        self.MainTextDisplay.text = ""
+//    }
+    
+    
+}
+
+
+extension UISegmentedControl {
+    func replaceSegments(segments: Array<String>) {
+        self.removeAllSegments()
+        for segment in segments {
+            self.insertSegment(withTitle: segment, at: self.numberOfSegments, animated: false)
+        }
+    }
+}
